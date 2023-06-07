5023cb8c61a0c571be103490e1d656e79694c195
Kevin Frei <kevinfrei@hotmail.com>
2023-06-06 21:55:47 -0700

    Updated dependencies, added some minor things

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-
+window.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  if (root){
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,root
+    )  
+  }
+})
 postMessage({ payload: 'removeLoading' }, '*')
